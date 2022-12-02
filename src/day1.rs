cb4b1b7bc77544ad6b821a5533ac73d0af260a7b
Gregory Kent <gregokent@gmail.com>
2022-12-01 21:10:51 -0500

    completed day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,48 @@
 use std::env;
+use std::io::BufRead;
 
 use crate::Result;
 
 pub fn run() -> Result<()> {
     let input = std::fs::File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/input/day1/input"))?;
 
-    println!("{input:?}");
+    let mut elves = Vec::new();
+
+    let mut calories = Vec::new();
+    let reader = std::io::BufReader::new(input);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.is_empty() {
+            elves.push(calories.clone());
+            calories.clear();
+            continue;
+        }
+
+        let val = line.parse::<u32>()?;
+        calories.push(val);
+    }
+
+    let elves_totals = elves
+        .iter()
+        .map(|cals| cals.iter().sum())
+        .collect::<Vec<u32>>();
+
+    puzzle1(elves_totals.clone())?;
+    puzzle2(elves_totals.clone())?;
     Ok(())
 }
 
-fn puzzle1() -> Result<()> {
+fn puzzle1(elves: Vec<u32>) -> Result<()> {
+    let max: u32 = *elves.iter().max().unwrap();
+    println!("puzzle 1: {}", max);
+    Ok(())
+}
+
+fn puzzle2(mut elves: Vec<u32>) -> Result<()> {
+    elves.sort();
+
+    let top_3: u32 = elves.iter().rev().take(3).sum();
+
+    println!("puzzle 2: {}", top_3);
     Ok(())
 }
